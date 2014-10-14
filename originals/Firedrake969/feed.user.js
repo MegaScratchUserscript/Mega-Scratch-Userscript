18c965fd44b96b0f3816537cff11b081fb000f8b
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-10-14 17:18:26 -0600

    Update feed.user.js

--- originals/Firedrake969/feed.user.js
+++ originals/Firedrake969/feed.user.js
@@ -5,6 +5,7 @@
 // @grant		none
 // ==/UserScript==
 
+//great thanks to PullJosh's MessageFilter.user.js for guidance.
 
 //"love", "share", "favorite", "remix", "following", "curator", "manager"
 var hide = ["remix", "following", "curator", "manager"];
@@ -32,3 +33,25 @@ if ($.inArray('following', hide) > -1) {
 if ($.inArray('curator', hide) > -1) {
   $( "#activity-feed li:contains('became a curator of')" ).css( "display", "none" );
 }
+
+if ($.inArray('manager', hide) > -1) {
+  $( "#activity-feed li:contains('was promoted to manager of')" ).css( "display", "none" );
+}
+
+$("#whats-happenin .box-head").append( "<span class='notice'>Some messages hidden</span>" )
+
+$(".notice").css({
+  "color":"#aaa",
+  "font-size":"12px",
+  "text-shadow":"none",
+  "font-weight":"400",
+  "line-height":"0", "float":"right",
+  "margin-top":"13px",
+  "cursor":"pointer",
+  "right":"0px"
+});
+
+$(".notice").click(function() {
+  $( "#activity-feed li" ).css( "display", "list-item" );
+  $(this).css( "display", "none" );
+});
