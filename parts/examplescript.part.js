c6493fb328127d33f420c4f7ad22df4f3cae6567
MegaApuTurkUltra <apu@aputurk.tk>
2014-10-15 08:16:26 -0400

    Update examplescript.part.js

--- parts/examplescript.part.js
+++ parts/examplescript.part.js
@@ -1,1 +1,5 @@
-ScratchUserscript.register("Example", "Example", []);
+// name, description, settings (to be implemented), init function
+ScratchUserscript.register("Example", "Example", [], function(){
+    // do stuffs here
+    // will be called on document.ready so you can manupulate dom here
+});
