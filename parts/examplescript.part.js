464bac688d8d78d6427ef674fd56f639969c0f79
MegaApuTurkUltra <apu@aputurk.tk>
2014-10-15 08:21:56 -0400

    Nooo typo

--- parts/examplescript.part.js
+++ parts/examplescript.part.js
@@ -1,5 +1,5 @@
 // name, description, settings (to be implemented), init function
 ScratchUserscript.register("Example", "Example", [], function(){
     // do stuffs here
-    // will be called on document.ready so you can manupulate dom here
+    // will be called on document.ready so you can manipulate dom here
 });
