acc1824e5dccfd08fcc9dea48c38897244589c96
MegaApuTurkUltra <apu@aputurk.tk>
2014-10-17 19:24:36 -0400

    Update to use unsafeWindow (part 2 of 2)

--- parts/examplescript.part.js
+++ parts/examplescript.part.js
@@ -1,5 +1,13 @@
-// start with examplescript = function
-examplescript = function(ScratchUserscript){
+// ==UserScript==
+// @name		Mega Scratch Userscript (part: example)
+// @author		MegaScratchUserscript 
+// @description	Example part
+// @include		http://scratch.mit.edu/*
+// @version		0.1
+// @grant		unsafeWindow
+// @icon		http://blue.gwiddle.org/img/MegaScratchUserscript65.png
+// ==/UserScript==
+unsafeWindow.exampleScript = function(ScratchUserscript){
 	console.log("Example started!");
 	// name, description, settings (to be implemented)
 	ScratchUserscript.registerPart("Example", "Example", []);
