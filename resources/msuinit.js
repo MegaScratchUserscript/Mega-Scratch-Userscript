4defcdde057b26bf6c222437bfd3631239fd874d
MegaApuTurkUltra <apu@aputurk.tk>
2014-10-29 07:13:08 -0400

    Update msuinit.js
    Maybe this will fix the settings dialog bug

--- resources/msuinit.js
+++ resources/msuinit.js
@@ -1,2 +1,5 @@
 // initializer, creates the unsafeWindow.msuParts object
 unsafeWindow.msuParts = {};
+// access window.jQuery instead of whatever jquery Greasemonkey may be putting in
+$ = unsafeWindow.$;
+jQuery = unsafeWindow.jQuery;
