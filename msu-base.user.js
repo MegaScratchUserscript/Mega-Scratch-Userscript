6d489f5ef9b0508e26308167a9896159f91e8ca1
GRA0007 <benjamin.grant@wesleycollege.net>
2014-11-10 16:07:44 +1100

    Change version for auto-update

--- msu-base.user.js
+++ msu-base.user.js
@@ -3,7 +3,7 @@
 // @author		MegaScratchUserscript (collab with some Scratch ATers: https://github.com/MegaScratchUserscript/Mega-Scratch-Userscript#-mega-scratch-userscript)
 // @description	A mega userscript with tons of epic uses!
 // @include		http://scratch.mit.edu/*
-// @version		0.2.1
+// @version		0.2.2
 // @grant		unsafeWindow
 // @grant		GM_getResourceText
 // @grant		GM_addStyle
