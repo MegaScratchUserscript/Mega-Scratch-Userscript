47c70bd2e18e8b48a4ff489291adee0acd3c0fc9
MegaApuTurkUltra <apu@aputurk.tk>
2014-11-11 11:02:02 -0500

    Change version to 1.0.0 for first release

--- msu-base.user.js
+++ msu-base.user.js
@@ -3,7 +3,7 @@
 // @author		MegaScratchUserscript (collab with some Scratch ATers: https://github.com/MegaScratchUserscript/Mega-Scratch-Userscript#-mega-scratch-userscript)
 // @description	A mega userscript with tons of epic uses!
 // @include		http://scratch.mit.edu/*
-// @version		0.2.4
+// @version		1.0.0
 // @grant		unsafeWindow
 // @grant		GM_getResourceText
 // @grant		GM_addStyle
