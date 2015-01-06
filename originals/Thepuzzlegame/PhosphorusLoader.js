d9897398cc8cfbe165aca3a57545980f47cb5910
Thepuzzlegame <sethmiller464@yahoo.com>
2015-01-05 19:07:25 -0800

    Update PhosphorusLoader.js
    Reordered

--- originals/Thepuzzlegame/PhosphorusLoader.js
+++ originals/Thepuzzlegame/PhosphorusLoader.js
@@ -11,4 +11,4 @@ script.type = 'text/javascript';
 script.src = 'https://raw.githubusercontent.com/MegaScratchUserscript/Mega-Scratch-Userscript/master/originals/Thepuzzlegame/externalSource.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-$( ".active" ).append( '<div id="load-with-pho" class="action tooltip bottom" style=""><span class="hovertext"><span class="arrow"></span>Load this project with Phosphorus</span><span onclick="loadWithPho()";>Phosphorus</span></div>' );
+$( ".active" ).append( '<div id="load-with-pho" class="action tooltip bottom" style="" onclick="loadWithPho()";><span class="hovertext"><span class="arrow"></span>Load this project with Phosphorus</span><span>Phosphorus</span></div>' );
