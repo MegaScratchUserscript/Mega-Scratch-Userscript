4044820599edbd758b864716e3eafaf704aeeb64
Thepuzzlegame <sethmiller464@yahoo.com>
2015-01-05 21:20:23 -0800

    Update PhosphorusLoader.js
    Last commit I promise :|

--- originals/Thepuzzlegame/PhosphorusLoader.js
+++ originals/Thepuzzlegame/PhosphorusLoader.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name        PhosphorusLoader
-// @namespace   pl
-// @include     http://scratch.mit.edu/projects/*/*
-// @version     0.1
-// @grant       none
+// @name PhosphorusLoader
+// @namespace pl
+// @include http://scratch.mit.edu/projects/*/*
+// @version 0.1
+// @grant none
 // ==/UserScript==
 
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = 'https://raw.githubusercontent.com/MegaScratchUserscript/Mega-Scratch-Userscript/master/originals/Thepuzzlegame/externalSource.js';
+script.src = 'https://raw.githubusercontent.com/MegaScratchUserscript/Mega-Scratch-Userscript/master/originals/Thepuzzlegame/externalJSSource.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-$( ".active" ).append( '<div id="load-with-pho" class="action tooltip bottom" style="" onclick="loadWithPho()";><span class="hovertext"><span class="arrow"></span>Load this project with Phosphorus</span><span>Phosphorus</span></div>' );
+$( ".active" ).append( '<div id="load-with-pho" class="action tooltip bottom" style="" onclick="loadPhos()";><span class="hovertext"><span class="arrow"></span>Load this project with Phosphorus</span><span>Phosphorus</span></div>' );
