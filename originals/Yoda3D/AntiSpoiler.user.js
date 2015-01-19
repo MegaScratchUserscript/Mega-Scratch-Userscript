88586fbffc6fdda4731ff4ced79e206113495c61
Samuel Hayden <pocketprayers@hotmail.com>
2015-01-19 10:47:02 -0500

    Fixed the spoiler script to work with "Spoiler"
    Fixed the spoiler script to work with "Spoiler", before it only worked with "spoiler".

--- originals/Yoda3D/AntiSpoiler.user.js
+++ originals/Yoda3D/AntiSpoiler.user.js
@@ -1,12 +1,12 @@
 /*Could someone add a setting for this, I can't figure out how.*/
 $(document).ready(function() { 
 	
-	$(".post_body_html:contains('spoiler')").hide();
+	$(".post_body_html:contains('spoiler'), .post_body_html:contains('Spoiler'), .post_body_html:contains('SPOILER')").hide();
 	
-	 $('<a class="reveal">Reveal spoilers &gt;&gt;</a> ').insertBefore(".post_body_html:contains('spoiler')");
+	 $('<a class="reveal">Reveal spoilers &gt;&gt;</a> ').insertBefore(".post_body_html:contains('spoiler'), .post_body_html:contains('Spoiler'), .post_body_html:contains('SPOILER')");
 
 	$("a.reveal").click(function(){
-		$(this).parents("div").children(".post_body_html:contains('spoiler')").fadeIn(2500);
+		$(this).parents("div").children(".post_body_html:contains('spoiler'), .post_body_html:contains('Spoiler'), .post_body_html:contains('SPOILER')").fadeIn(2500);
 		$(this).remove();
 	});
 
