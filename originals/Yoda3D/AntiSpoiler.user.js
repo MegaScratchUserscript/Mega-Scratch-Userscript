55fd81c9f3a0fc107234fcbab2f6de47aae57094
Samuel Hayden <pocketprayers@hotmail.com>
2015-01-18 19:12:11 -0500

    Requesting a setting

--- originals/Yoda3D/AntiSpoiler.user.js
+++ originals/Yoda3D/AntiSpoiler.user.js
@@ -1,3 +1,4 @@
+/*Could someone add a setting for this, I can't figure out how.*/
 $(document).ready(function() {
     $(".post_body_html:contains('spoiler')").html("This post contains spoilers (to disable spoiler protection; edit your MSU settings)");
 });
