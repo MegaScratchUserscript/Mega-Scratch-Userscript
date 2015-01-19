df1d14db2ac4460f2ccd71726e4e49a253b30352
Samuel Hayden <pocketprayers@hotmail.com>
2015-01-18 19:00:20 -0500

    Create AntiSpoiler.user.js
    This will block all posts that contain the word "spoiler".

---
+++ originals/Yoda3D/AntiSpoiler.user.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".post_body_html:contains('spoiler')").html("This post contains spoilers (to disable spoiler protection; edit your MSU settings)");
+});
+$(document).ready(function() {
+    $(".post_body_html:contains('spoilers')").html("This post contains spoilers (to disable spoiler protection; edit your MSU settings)");
+});
