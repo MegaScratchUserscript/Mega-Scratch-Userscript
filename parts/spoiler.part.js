ef7ed8fa561289a32e7b8463737b5e843f6ef26a
GrannyCookies <16batesa@gmail.com>
2015-01-22 20:07:47 +0000

    Update spoiler.part.js

--- parts/spoiler.part.js
+++ parts/spoiler.part.js
@@ -5,25 +5,24 @@ msuParts["spoiler"] = function (ScratchUserscript) {
 
 	var isEnabled = ScratchUserscript.isPartEnabled("spoiler");
 
-	if (!isEnabled)
-		return;
-
-	// actual script
-	$(document).ready(function() {
-		$('.postright span').each(function(index) {
-			if($(this).attr('style')==='color:spoiler') {
-				$('<a class="revealTag" id="openspoiler' + index + '">Open spoiler &gt;&gt;</a> ').insertBefore(this);
-				var make = $(this).html();
-				$(this).html('<span id="spoiler' + index + '">' + make + '</span>').hide();
-			}
+	if (isEnabled) {
+		// actual script
+		$(document).ready(function() {
+			$('.postright span').each(function(index) {
+				if($(this).attr('style')==='color:spoiler') {
+					$('<a class="revealTag" id="openspoiler' + index + '">Open spoiler &gt;&gt;</a> ').insertBefore(this);
+					var make = $(this).html();
+					$(this).html('<span id="spoiler' + index + '">' + make + '</span>').hide();
+				}
+			});
+	
+		    $("a.revealTag").click(function() {
+		    	var thing = $(this).attr('id').substring(11, $(this).attr('id').length);
+	
+		        $( '#spoiler' + thing ).parent().css('display', 'inherit');
+	
+		        $(this).remove();
+		    });
 		});
-
-	    $("a.revealTag").click(function() {
-	    	var thing = $(this).attr('id').substring(11, $(this).attr('id').length);
-
-	        $( '#spoiler' + thing ).parent().css('display', 'inherit');
-
-	        $(this).remove();
-	    });
-	});
+	}
 };
