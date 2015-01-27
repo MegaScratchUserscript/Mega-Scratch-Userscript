f9a0c880393d9b82c6612ac5fa07bf369f454ccc
Felizolinha <matheusbsb2009@Hotmail.com>
2015-01-27 21:48:53 -0200

    No project Top Bar & Phosphorus Loading
    Featured projects now automatically load with Phosphorus if iFrame page yields 404 (unshared and deleted projects).
    Project's top bar is now hidden as to leave more room for the project.
    
    PS: The css and height attribute changes are there to work around a glitch that cuts off the bottom of the iFrame.
    PS²: This is a merge from the PR I made to GRA0007's originals folder.

--- parts/profileenhancer.part.js
+++ parts/profileenhancer.part.js
@@ -116,8 +116,27 @@ msuParts["profileEnhancer"] = function (ScratchUserscript) {
 		if (settings.liveProject) {
 			//Make project live
 			$("#featured-project").remove();
-			var projID = unsafeWindow.Scratch.INIT_DATA.PROFILE.featuredProject.id;
-			$(".stage").append("<iframe allowtransparency='true' width='282' height='210' src='http://scratch.mit.edu/projects/embed/" + projID + "/?autostart=true' frameborder='0' allowfullscreen=''></iframe>");
+			var $projName = $('.project-name'),
+		            projID = unsafeWindow.Scratch.INIT_DATA.PROFILE.featuredProject.id;
+		            
+			 $projName.html($projName.html().trim()); //Make sure no whitespaces interefere with centering
+			 $('.player .title').css('text-align', 'center');
+
+			 $( ".stage" ).append( "<div style='overflow: hidden; height: 216px;' id='applet'><iframe style='margin-top: -25px;' allowtransparency='true' width='282' height='237' frameborder='0' allowfullscreen=''></iframe></div>" );
+
+			 $('.stage iframe').load(function(){
+			        var is404 = $(this).contents().find('#page-404').length > 0;
+			        if(is404) {
+			            $('.stage div').css('height', '211px');
+			            $(this).css('margin-top', '-14px');
+			            $(this).attr('height', '239');
+			        	$(this).attr('src', 'http://phosphorus.github.io/app.html?id=' + projID);
+			        }
+			        else if($(this).attr('src').indexOf('http://scratch.mit.edu/projects/embed/') > -1) {
+			            setTimeout(function(){$('.stage iframe').attr('height', '238');}, 200);
+			        }
+			 });
+			 $('.stage iframe').attr("src", "http://scratch.mit.edu/projects/embed/" + projID + "/?autostart=true");
 		}
 	}
 };
