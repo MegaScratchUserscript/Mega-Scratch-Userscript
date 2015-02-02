6f50d657f3589ab2b3109856099d3988bbddd907
Felizolinha <matheusbsb2009@Hotmail.com>
2015-02-01 23:39:12 -0200

    Added custom text for Wiki button

--- parts/wikilink.part.js
+++ parts/wikilink.part.js
@@ -5,10 +5,28 @@ msuParts["wikilink"] = function(ScratchUserscript){
 
 	var isEnabled = ScratchUserscript.isPartEnabled("wikilink");
 	
-	settingsDlg.append("<i>No additional settings (to disable this, use the checkbox in the left section).</i>");
+	var settings = {
+		name: "Wiki"
+	};
+	
+	settings = ScratchUserscript.readSetting("wikilink", "settings", settings);
+	
+	updateChanges = function(){
+		settings.name = $("#wiki-name").val();
+		ScratchUserscript.writeSetting("wikilink", "settings", settings);
+		
+		if(isEnabled) {
+			settings = ScratchUserscript.readSetting("wikilink", "settings", settings); //Update settings
+			$(".site-nav li a[href='http://wiki.scratch.mit.edu/']").html(settings.name); //Auto-update button
+		}
+	};
+	
+	settingsDlg.append('Button Name: <input type="text" id="wiki-name" value="' + settings.name + '">');
+	
+	settingsDlg.find("input").bind("change cut paste keyup", updateChanges);
 	
 	if(!isEnabled) return;
 	
 	// begin actual script
-	$('.site-nav li:nth-of-type(3)').after("<li><a href='http://wiki.scratch.mit.edu/'>Wiki</a></li>");
+	$('.site-nav li:nth-of-type(3)').after("<li><a href='http://wiki.scratch.mit.edu/'>" + settings.name + "</a></li>");
 };
